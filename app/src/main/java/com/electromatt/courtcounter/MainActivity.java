1bdfbf5297411eaa12b3663cbf1edc7379aa936b
matt <mdruckhammer@themeshnetworks.com>
2016-06-29 11:29:43 -0500

    Initial Commit

---
+++ app/src/main/java/com/electromatt/courtcounter/MainActivity.java
@@ -0,0 +1,62 @@
+package com.electromatt.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addOneForA(View view){
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addTwoForA(View view){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addThreeForA(View view){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addOneForB(View view){
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addTwoForB(View view){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addThreeForB(View view){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void resetScore(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+
+    }
+}
